97e0c7d2de5ad8d08841cd4d8e8fc8bd10d8cd8e
Grassi <grassiricardorg@gmail.com>
2017-06-24 00:19:37 -0300

    corrections and improvements in layout

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -57,6 +58,10 @@ export default class Todo extends Component {
             .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render() {
         return (
             <div>
@@ -65,7 +70,8 @@ export default class Todo extends Component {
                     description={this.state.description} 
                     handleAdd={this.handleAdd} 
                     handleChange={this.handleChange}
-                    handleSearch={this.handleSearch}/>
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}/>
                 <TodoList 
                     list={this.state.list} 
                     handleRemove={this.handleRemove}
